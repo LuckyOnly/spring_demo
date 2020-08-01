e5011feb3df448275a85c9fe094b760088acc571
zongfangfang <1047670763@qq.com>
2020-08-01 11:14:34 +0800

    spring 注解component

--- src/main/java/com/zff/dao/impl/AccountDaoImpl.java
+++ src/main/java/com/zff/dao/impl/AccountDaoImpl.java
@@ -1,7 +1,10 @@
 package com.zff.dao.impl;
 
 import com.zff.dao.AccountDao;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@Repository
 public class AccountDaoImpl implements AccountDao {
     public void saveAccount() {
         System.out.println("save data");
--- src/main/java/com/zff/ui/Client.java
+++ src/main/java/com/zff/ui/Client.java
@@ -9,16 +9,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Client {
     public static void main(String[] args) {
 //        获取容器
-        ApplicationContext ac= new ClassPathXmlApplicationContext("bean.xml");
+        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
 
         //Component 注解默认取类名且首字母小写
-        AccountService as3 = (AccountService)ac.getBean("accountServiceImpl");
+        AccountService as3 = (AccountService) ac.getBean("accountServiceImpl");
         as3.saveAccount();
-        /**        AccountService as = (AccountService)ac.getBean("accountService");
-        AccountDao adao = ac.getBean("accountDao",AccountDao.class);
-        System.out.println(as);
+        /**
+         * AccountService as = (AccountService)ac.getBean("accountService");
+         * as.saveAccount();
+         * System.out.println(as);
+         * */
+        AccountDao adao = ac.getBean("accountDaoImpl", AccountDao.class);
+
         System.out.println(adao);
-        as.saveAccount();
-         */
+
+
     }
 }
